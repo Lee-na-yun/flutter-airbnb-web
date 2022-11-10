dd4f14d5791f6a60567dd17884740758e9057622
Lee-na-yun <happyin3333@nate.com>
2022-11-10 15:21:22 +0900

    form에 title 적기 완료

--- lib/components/home/home_header_form.dart
+++ lib/components/home/home_header_form.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_airbnb/components/common/common_form_field.dart';
 import 'package:flutter_airbnb/size.dart';
+import 'package:flutter_airbnb/styles.dart';
 
 // 202p 4번
 class HomeHeaderForm extends StatelessWidget {
@@ -13,7 +14,6 @@ class HomeHeaderForm extends StatelessWidget {
       alignment: Alignment(-0.6, 0),
       child: Container(
         width: 420,
-        height: 200,
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(20),
@@ -33,12 +33,26 @@ class HomeHeaderForm extends StatelessWidget {
     );
   }
 
-  SizedBox _buildFormTitle() {
-    return SizedBox();
+  Widget _buildFormTitle() {
+    return Column(
+      children: [
+        Text(
+          "모두의 숙소를 검색하세요.",
+          style: h4(),
+        ),
+        SizedBox(height: gap_xs),
+        Text(
+          "혼자하는 여행에 적합한 개인실부터 여럿이 함께하는 여행에 좋은 '공간전체' 숙소까지, 모두의 숙소에 다 있습니다.",
+          style: body1(),
+        ),
+        SizedBox(height: gap_m),
+      ],
+    );
   }
 
   CommonFormField _buildFormField() {
-    return CommonFormField();
+    // 컴포넌트화 됨
+    return CommonFormField(prefixText: "위치", hintText: "근처 추천 장소");
   }
 
   SizedBox _buildFormSubmit() {
