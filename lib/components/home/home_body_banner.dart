b59f89491ccd442765638329381a67a5c9fe27be
Lee-na-yun <happyin3333@nate.com>
2022-11-10 16:34:16 +0900

    body 이미지 배너 디자인 완료

--- lib/components/home/home_body_banner.dart
+++ lib/components/home/home_body_banner.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_airbnb/components/common/basic_button.dart';
 import 'package:flutter_airbnb/size.dart';
+import 'package:flutter_airbnb/styles.dart';
 
 class HomeBodyBanner extends StatelessWidget {
   const HomeBodyBanner({Key? key}) : super(key: key);
@@ -18,10 +20,50 @@ class HomeBodyBanner extends StatelessWidget {
   }
 
   _buildBannerImage() {
-    return SizedBox();
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Image.asset(
+        "assets/banner.jpg",
+        fit: BoxFit.cover,
+        width: double.infinity,
+        height: 320,
+      ),
+    );
   }
 
   _buildBannerCaption() {
-    return SizedBox();
+    return Positioned(
+      left: 40,
+      top: 40,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            constraints: BoxConstraints(
+              maxWidth: 250,
+            ),
+            child: Text(
+              "이제, 여행은 가까운 곳에서",
+              style: h4(mColor: Colors.white),
+            ),
+          ),
+          SizedBox(height: gap_m),
+          Container(
+            constraints: BoxConstraints(
+              maxWidth: 250,
+            ),
+            child: Text(
+              "새로운 공간에 머물러 보세요. 살아보기, 출장, 여행 등 다양한 목적에 맞는 숙소를 찾아보세요.",
+              style: subtitleBig(mColor: Colors.white),
+            ),
+          ),
+          SizedBox(height: gap_m),
+          SizedBox(
+            width: 170,
+            child: BasicButton(text: "가까운 여행지 둘러보기"),
+          ),
+        ],
+      ),
+    );
   }
 }
